f0e7553e7fedb2950bd8d0263513a1c98a264dac
Rufus Wong <rcywongaa@gmail.com>
2020-06-26 16:50:01 +0800

    Load atlas and simulate

---
+++ load_atlas.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+from pydrake.common import FindResourceOrThrow
+from pydrake.math import RigidTransform
+from pydrake.multibody.parsing import Parser
+from pydrake.systems.analysis import Simulator
+from pydrake.all import MultibodyPlant
+from pydrake.systems.framework import DiagramBuilder
+from pydrake.geometry import ConnectDrakeVisualizer, SceneGraph, HalfSpace, Box
+from pydrake.multibody.plant import MultibodyPlant, AddMultibodyPlantSceneGraph, CoulombFriction
+from pydrake.systems.analysis import Simulator
+from pydrake.solvers.mathematicalprogram import MathematicalProgram, Solve
+from pydrake.math import RollPitchYaw
+
+builder = DiagramBuilder()
+plant, scene_graph = AddMultibodyPlantSceneGraph(builder, MultibodyPlant(1.0e-3))
+
+atlas_file = FindResourceOrThrow("drake/examples/atlas/urdf/atlas_convex_hull.urdf")
+atlas = Parser(plant).AddModelFromFile(atlas_file)
+
+static_friction = 1.0
+green = np.array([0.5, 1.0, 0.5, 1.0])
+
+plant.RegisterVisualGeometry(plant.world_body(), RigidTransform(), HalfSpace(),
+        "GroundVisuaGeometry", green)
+
+ground_friction = CoulombFriction(1.0, 1.0)
+plant.RegisterCollisionGeometry(plant.world_body(), RigidTransform(), HalfSpace(),
+        "GroundCollisionGeometry", ground_friction)
+
+plant.Finalize()
+plant.set_penetration_allowance(1.0e-3)
+plant.set_stiction_tolerance(1.0e-3)
+
+ConnectDrakeVisualizer(builder=builder, scene_graph=scene_graph)
+diagram = builder.Build()
+
+diagram_context = diagram.CreateDefaultContext()
+plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
+
+tau = np.zeros(plant.num_actuated_dofs())
+plant.get_actuation_input_port().FixValue(plant_context, tau)
+
+pelvis = plant.GetBodyByName("pelvis")
+X_WP = RigidTransform(RollPitchYaw(0.0, 0.0, 0.0), np.array([0.0, 0.0, 0.95]))
+plant.SetFreeBodyPose(plant_context, pelvis, X_WP)
+
+simulator = Simulator(diagram, diagram_context)
+simulator.set_target_realtime_rate(0.2)
+simulator.AdvanceTo(2.0)
